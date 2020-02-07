5cfadef5e905c56baa3c577a8efe049a705f1963
sy <sssyyw777@gmail.com>
2020-02-07 14:50:02 +0800

    version 1

---
+++ src/main/java/com/sy/test/jenkins/TestController.java
@@ -0,0 +1,15 @@
+package com.sy.test.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String index() {
+        return "version 1";
+    }
+}
